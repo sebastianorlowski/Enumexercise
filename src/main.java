82402c3b9249ff859dcc99549c017c65e1acea67
mydarkside <sebekovsky@gmail.com>
2020-01-24 19:30:27 +0100

    Enum switch exercise

--- src/main.java
+++ src/main.java
@@ -1,16 +1,57 @@
 import java.awt.*;
+import java.util.Scanner;
 
 
 public class main {
 
     public static void main(String[] args) {
-        for(TshirtSize tshirtSize : TshirtSize.values()) {
-            System.out.println("Number of object: " + (tshirtSize.ordinal()+1));
-            System.out.println("His name: " + tshirtSize.name());
-            System.out.println("His height: " + tshirtSize.getHeight());
-            System.out.println("His chest length: " + tshirtSize.getChestLength());
-            System.out.println("His waist length: " + tshirtSize.getWaistLength());
+        boolean appOn = true;
+        Scanner scan = new Scanner(System.in);
 
+        for(TshirtSize tshirtSize : TshirtSize.values()) {
+            System.out.println("T shirt: " + (tshirtSize.ordinal()+1) + ". " + tshirtSize.name() + ", " + tshirtSize.getHeight() + ", " + tshirtSize.getChestLength() + ", " + tshirtSize.getWaistLength());
+                    }
+
+        while(appOn) {
+            String size;
+            System.out.println("Please give my name of size");
+            System.out.println("You can choose XS, S, M, L, XL");
+            System.out.println("Please enter 0 if you want to exit");
+            size = scan.nextLine();
+
+
+            switch (size.toUpperCase()) {
+
+                case "XS":
+                    System.out.println((TshirtSize.XS.ordinal()+1) + " " + TshirtSize.XS.name());
+                    break;
+
+                case "S":
+                    System.out.println((TshirtSize.S.ordinal()+1) + " " + TshirtSize.S.name());
+                    break;
+
+                case "M":
+                    System.out.println((TshirtSize.M.ordinal()+1) + " " + TshirtSize.M.name());
+                    break;
+
+                case "L":
+                    System.out.println((TshirtSize.L.ordinal()+1) + " " + TshirtSize.L.name());
+                    break;
+
+                case "XL":
+                    System.out.println((TshirtSize.XL.ordinal()+1) + " " + TshirtSize.XL.name());
+                    break;
+
+                case "0":
+                    appOn = false;
+                    System.exit(0);
+
+                default:
+                    System.out.println("Wrong letter!");
+                    break;
+
+
+            }
         }
     }
 
